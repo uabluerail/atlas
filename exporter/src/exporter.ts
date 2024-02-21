c1af5deb81fa9d091e410a01be148e139c4a7e87
Max Ignatenko <gelraen.ua@gmail.com>
2024-02-21 17:13:54 +0000

    EVEN GODS BEND TO MY WILL, MWAHAHAHAHAHAHAHA

--- exporter/src/exporter.ts
+++ exporter/src/exporter.ts
@@ -33,6 +33,7 @@ interface IndexNode {
   key: number;
   did: string;
   label: string;
+  community: number;
 }
 
 interface Cluster {
@@ -210,6 +211,7 @@ fetchGraph(process.argv[2]).then((graphData: { edges: Edge[]; nodes: Node[] }) =
       key: i,
       did: node.did,
       label: node.handle,
+      community: node.community,
     };
     const graphNode = {
       key: i,
@@ -283,6 +285,75 @@ fetchGraph(process.argv[2]).then((graphData: { edges: Edge[]; nodes: Node[] }) =
   }
 
   log("Done adding edges");
+
+
+  const communitiesGraph = new MultiDirectedGraph();
+  const communities:Map<number,ClusterRepPrio> = new Map();
+
+  for (let i = 0; i < totalNodes; i++) {
+    const node = nodes[i];
+    if (!communities.has(node.community)) {
+      communities.set(node.community, { label: "", prio: 0 });
+    }
+    const rep = clusterRepresentatives.get(node.handle);
+    if (rep !== undefined) {
+      if (!communities.has(node.community) || communities.get(node.community)!.prio < rep.prio) {
+        communities.set(node.community, rep)
+      }
+    }
+  }
+
+  for (const [community, rep] of communities) {
+    communitiesGraph.addNode(community, {
+      key: community,
+      label: rep.label,
+      community: community,
+    });
+  }
+
+  for (let i = 0; i < totalEdges; i++) {
+    const edge = edges[i];
+    const source = indexNodes.get(edge.source)?.community!;
+    const target = indexNodes.get(edge.target)?.community!;
+    if (source == target) continue;
+    const graphEdge = communitiesGraph.findEdge(source,target, ()=>true);
+    if (graphEdge) {
+      communitiesGraph.updateEdgeAttribute(graphEdge, 'weight', v => v + edge.weight);
+    } else {
+      communitiesGraph.addEdge(source, target, {weight: edge.weight});
+    }
+  }
+
+  communitiesGraph.forEachEdge((edge, attrs) => communitiesGraph.updateEdgeAttribute(edge, 'weight', v => Math.log(v)));
+
+  circular.assign(communitiesGraph);
+  const uaNode = communitiesGraph.findNode((node, attrs) => attrs.label == "ua");
+  communitiesGraph.updateNode(uaNode, (attrs) => ({
+    ...attrs,
+    fixed: true,
+    x: 0,
+    y: 0,
+  }));
+
+  forceAtlas2.assign(communitiesGraph, {
+    settings: {
+    ...forceAtlas2.inferSettings(communitiesGraph),
+    },
+    iterations: 1000,
+  });
+
+  // Flip to true to export communities layout
+  if (false) {
+    communitiesGraph.setAttribute("clusters", Array.from(communities.keys()).map((c) => ({
+      label: communities.get(c)!.label,
+      idx: c,
+      size: 1,
+    })));
+    communitiesGraph.setAttribute("lastUpdated", new Date().toISOString());
+
+    fs.writeFileSync("./out/exported_graph_enriched.json", JSON.stringify(communitiesGraph.export()));
+    return;
+  }
 
   // const degrees = graph.nodes().map((node) => graph.degree(node));
   // const minDegree = degrees.reduce((a, b) => Math.min(a, b));
@@ -404,7 +475,22 @@ fetchGraph(process.argv[2]).then((graphData: { edges: Edge[]; nodes: Node[] }) =
 
   log("Assigning layout...");
 
+  const communityLocation: Map<number, { x: number, y: number }> = new Map();
+  communitiesGraph.forEachNode((_, attrs) => {
+    communityLocation.set(attrs.key, { x: attrs.x, y: attrs.y });
+  });
   circular.assign(graph);
+  graph.forEachNode((n, { community }) => {
+    if (community !== undefined) {
+      const loc = communityLocation.get(community);
+      if (loc) {
+        // Scale down and shift to cluster location.
+        graph.updateNodeAttribute(n, 'x', x => x/100 + loc.x);
+        graph.updateNodeAttribute(n, 'y', y => y/100 + loc.y);
+      }
+    }
+  });
+
   const settings = forceAtlas2.inferSettings(graph);
   const iterationCount = atlasMode.iterationCount;
 
@@ -457,9 +543,9 @@ fetchGraph(process.argv[2]).then((graphData: { edges: Edge[]; nodes: Node[] }) =
   forceAtlas2.assign(graph, { settings, iterations: iterationCount });
   log("Done running Force Atlas");
 
-  log(`Rotating Force Atlas...`);
-  rotation.assign(graph, - 2 * Math.PI / 7);
-  log("Successfully rotated Atlas");
+  // log(`Rotating Force Atlas...`);
+  // rotation.assign(graph, - 2 * Math.PI / 7);
+  // log("Successfully rotated Atlas");
 
   // initialize clusters from graph data
   const communityClusters: { [key: string]: Cluster } = {};
