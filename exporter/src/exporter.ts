3d90902db9ac252c092efe1e7762d2c5fc25d5f9
mathan <yuliya.kramarenko@gmail.com>
2024-03-03 12:54:57 -0800

    Added Layers.

--- exporter/src/exporter.ts
+++ exporter/src/exporter.ts
@@ -51,8 +51,13 @@ interface Cluster {
   positions: { x: number; y: number }[];
 }
 
+enum Layer {
+  MAIN, MODERATION, NONE
+}
+
 interface ClusterRepPrio {
   label: string;
+  layer: Layer;
   prio: number;
   displayName?: string;
   dbIndex?: number;
@@ -115,11 +120,13 @@ const atlasLayout = layoutSettings.dual;
 
 clusterRepresentatives.set("uabluerail.org", {
   label: "ua-yellow",
+  layer: Layer.MAIN,
   displayName: "🇺🇦🟡 Жовті",
   prio: 5,
 });
 clusterRepresentatives.set("wormwoodstar.bsky.social", {
   label: "ua-blue",
+  layer: Layer.MAIN,
   displayName: "🇺🇦📘 Сині",
   prio: 4,
 });
@@ -128,6 +135,7 @@ clusterRepresentatives.set("wormwoodstar.bsky.social", {
 //check representative every time
 clusterRepresentatives.set("hto-ya.bsky.social", {
   label: "ua-extended",
+  layer: Layer.MAIN,
   prio: 5,
 });
 
@@ -135,125 +143,156 @@ clusterRepresentatives.set("hto-ya.bsky.social", {
 //check representative every time
 clusterRepresentatives.set("bsky.church", {
   label: "ua-church",
+  layer: Layer.MAIN,
   prio: 6,
 });
 clusterRepresentatives.set("mohican.tech", {
   label: "ua-fun",
+  layer: Layer.MAIN,
   prio: 6,
 });
 clusterRepresentatives.set("gniv.bsky.social", {
   label: "ua-art",
+  layer: Layer.MAIN,
   prio: 6,
 });
 clusterRepresentatives.set("takeawaynoise.bsky.social", {
   label: "ua-lgbtqa",
+  layer: Layer.MAIN,
   prio: 6,
 });
 clusterRepresentatives.set("kanadenka.bsky.social", {
   label: "ua-write",
+  layer: Layer.MAIN,
   prio: 6,
 });
 clusterRepresentatives.set("holyagnostic.bsky.social", {
   label: "ua-gaming",
+  layer: Layer.MAIN,
   prio: 6,
 });
 clusterRepresentatives.set("isimon.bsky.social", {
   label: "ua-tech",
+  layer: Layer.MAIN,
   prio: 6,
 });
 clusterRepresentatives.set("paperpllant.bsky.social", {
   label: "ua-kpop",
+  layer: Layer.MAIN,
   prio: 6,
 });
 
 //fixes
 clusterRepresentatives.set("oyin.bo", {
   label: "ua-1",
+  layer: Layer.MAIN,
   prio: 6,
 });
 clusterRepresentatives.set("tyrrrz.me", {
   label: "ua-2",
+  layer: Layer.MAIN,
   prio: 6,
+});
+
+clusterRepresentatives.set("hardrockfella.bsky.social", {
+  label: "nafo",
+  layer: Layer.MAIN,
+  prio: 3,
+});
+
+//underlay clusters
+//check representative every time
+clusterRepresentatives.set("larsen256.bsky.social", {
+  label: "ua-boroshno-extended",
+  layer: Layer.MODERATION,
+  prio: 3,
+});
+//check representative every time
+clusterRepresentatives.set("kyrylowozniak.bsky.social", {
+  label: "nafo-extended",
+  layer: Layer.MODERATION,
+  prio: 3,
 });
 
 clusterRepresentatives.set("publeecist.bsky.social", {
   label: "ua-boroshno",
+  layer: Layer.MODERATION,
   prio: 3,
 });
 clusterRepresentatives.set("metronom.bsky.social", {
   label: "be",
+  layer: Layer.MODERATION,
   prio: 3,
 });
 clusterRepresentatives.set("tinaarishina.bsky.social", {
   label: "ru-other",
+  layer: Layer.MODERATION,
   prio: 4,
 });
 clusterRepresentatives.set("alphyna.bsky.social", {
   label: "ru",
+  layer: Layer.MODERATION,
   prio: 4,
-});
-clusterRepresentatives.set("hardrockfella.bsky.social", {
-  label: "nafo",
-  prio: 3,
 });
 
 //underlay clusters
 //check representative every time
 clusterRepresentatives.set("ffuuugor.bsky.social", {
   label: "ru-extended",
+  layer: Layer.MODERATION,
   prio: 4,
 });
 //check representative every time
 clusterRepresentatives.set("shurikidze.bsky.social", {
   label: "be-extended",
-  prio: 3,
-});
-//check representative every time
-clusterRepresentatives.set("larsen256.bsky.social", {
-  label: "ua-boroshno-extended",
-  prio: 3,
-});
-//check representative every time
-clusterRepresentatives.set("kyrylowozniak.bsky.social", {
-  label: "nafo-extended",
+  layer: Layer.MODERATION,
   prio: 3,
 });
 
 //control clusters
 clusterRepresentatives.set("killustration.bsky.social", {
   label: "artists",
+  layer: Layer.MAIN,
   prio: 3,
 });
 clusterRepresentatives.set("jalpari.bsky.social", {
   label: "writers",
+  layer: Layer.MAIN,
   prio: 3,
 });
 clusterRepresentatives.set("cactimutt.bsky.social", {
   label: "furry",
+  layer: Layer.MAIN,
   prio: 3,
 });
 clusterRepresentatives.set("malwarejake.bsky.social", {
   label: "infosec",
+  layer: Layer.MAIN,
   prio: 3,
 });
 clusterRepresentatives.set("lookitup.baby", {
   label: "tech",
+  layer: Layer.MAIN,
   prio: 4,
 });
 clusterRepresentatives.set("pfrazee.com", {
   label: "startup",
+  layer: Layer.MAIN,
   prio: 5,
 });
 clusterRepresentatives.set("bsky.app", {
   label: "en-meta",
+  layer: Layer.MAIN,
   prio: 5,
 });
 clusterRepresentatives.set("gamedevlist.bsky.social", {
   label: "gamers",
+  layer: Layer.MAIN,
   prio: 3,
 });
 clusterRepresentatives.set("onsu.re", {
   label: "web3",
+  layer: Layer.MAIN,
   prio: 3,
 });
 
@@ -392,7 +431,7 @@ fetchGraph(process.argv[2]).then((graphData: { edges: Edge[]; nodes: Node[], tim
     for (let i = 0; i < totalNodes; i++) {
       const node = nodes[i];
       if (!communities.has(node.community)) {
-        communities.set(node.community, { label: "", prio: 0 });
+        communities.set(node.community, { label: "", layer: Layer.NONE, prio: 0 });
       }
       const rep = clusterRepresentatives.get(node.handle);
       if (rep !== undefined) {
